617d9cd20a2e2dac38aeea19ea99c6a2bbf7d725
Eduard-Valentin Cinca <eduard_cinca@yahoo.com>
2024-02-19 18:32:23 +0200

    modified the views for creating rooms

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from rest_framework import serializers
 from .models import Room
 
@@ -7,3 +8,8 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause',
                   'votes_to_skip', 'created_at')
+        
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
